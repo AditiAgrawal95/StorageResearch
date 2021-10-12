965a75598b9753725241e26629a6829bacc9e88c
AlexWolski <alex.theinfinite@gmail.com>
2021-10-12 15:15:27 -0700

    Modified APFS parsing methods to return a struct pointer

--- apfs.c
+++ apfs.c
@@ -1,83 +1,88 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "apfs.h"
 
-int parse_blk_header(FILE *apfs)
+APFS_BH* parse_blk_header(FILE *apfs)
 {
-	APFS_BH block_header = {0};
+	APFS_BH *block_header = (APFS_BH*) malloc(sizeof(APFS_BH));
 	unsigned int size = 0;
 
-	if ((size = fread(&block_header, 1, sizeof(block_header), apfs)) < 0) {
-		printf("Error reading from APFS File! [%d] size = %lu\n", size, sizeof(block_header));
-		return -1;
+	if ((size = fread(block_header, 1, sizeof(*block_header), apfs)) < 0) {
+		printf("Error reading from APFS File! [%d] size = %lu\n", size, sizeof(*block_header));
+		return NULL;
 	}
 
 	printf("\nParsed APFS BLK Header! Size = %u\n", size);
 	printf("HEADER\n");
 	printf("-------------------------------------\n");
-	printf("Checksum = %lu\n", block_header.checksum);
-	printf("Block ID = %lu\n", block_header.block_id);
-	printf("Version = %lu\n", block_header.version);
-	printf("Block Type = 0x%X\n", block_header.block_type);
-	printf("Flags = 0x%X\n", block_header.flags);
-	printf("Sub Type = 0x%X\n", block_header.sub_type);
-	printf("Padding = 0x%X\n", block_header.padding);
+	printf("Checksum = %lu\n", block_header->checksum);
+	printf("Block ID = %lu\n", block_header->block_id);
+	printf("Version = %lu\n", block_header->version);
+	printf("Block Type = 0x%X\n", block_header->block_type);
+	printf("Flags = 0x%X\n", block_header->flags);
+	printf("Sub Type = 0x%X\n", block_header->sub_type);
+	printf("Padding = 0x%X\n", block_header->padding);
 	printf("-------------------------------------\n");
 
-	return 0;
+	return block_header;
 }
 
-int parse_super_blk(FILE *apfs)
+APFS_SuperBlk* parse_super_blk(FILE *apfs)
 {
-	APFS_SuperBlk super_blk = {0};
+	APFS_SuperBlk *super_blk = (APFS_SuperBlk*) malloc(sizeof(APFS_SuperBlk));
 	unsigned int size = 0;
 
-	if ((size = fread(&super_blk, 1, sizeof(super_blk), apfs)) < 0) {
-		printf("Error reading from APFS File! [%d] size = %lu\n", size, sizeof(super_blk));
-		return -1;
+	if ((size = fread(super_blk, 1, sizeof(*super_blk), apfs)) < 0) {
+		printf("Error reading from APFS File! [%d] size = %lu\n", size, sizeof(*super_blk));
+		return NULL;
 	}
 
 	printf("\nParsed Super Block Header! Size - %u\n", size);
 	printf("SUPER BLOCK DATA\n");
 	printf("-------------------------------------\n");
-	printf("BlockSize - %u\n", super_blk.BlockSize);
-	printf("BlocksCount - %lu\n", super_blk.BlocksCount);
-	printf("Features - %lu\n", super_blk.Features);
-	printf("ReadOnlyFeatures - %lu\n", super_blk.ReadOnlyFeatures);
-	printf("IncompatibleFeatures - %lu\n", super_blk.IncompatibleFeatures);
-	printf("NextIndent - %lu\n", super_blk.NextIdent);
-	printf("NextTransaction- %lu\n", super_blk.NextTransaction);
-	printf("Desc Blocks - %u\n", super_blk.DescriptorBlocks);
-	printf("Data Blocks - %u\n", super_blk.DataBlocks);
-	printf("DescriptorBase - %lu\n", super_blk.DescriptorBase);
-	printf("DataBase - %lu\n", super_blk.DataBase);
-	printf("DescriptorNext - %u\n", super_blk.DescriptorNext);
-	printf("DataNext - %u\n", super_blk.DataNext);
-	printf("DescriptorIndex - %u\n", super_blk.DescriptorIndex);
-	printf("DescriptorLength - %u\n", super_blk.DescriptorLength);
-	printf("DataIndex - %u\n", super_blk.DataIndex);
-	printf("DataLength - %u\n", super_blk.DataLength);
-	printf("SpaceManagerIdent - %lu\n", super_blk.SpaceManagerIdent);
-	printf("ObjectsMapIdent - %lu\n", super_blk.ObjectsMapIdent);
-	printf("ReaperIdent - %lu\n", super_blk.ReaperIdent);
-	printf("MaximumVolumes - %u\n", super_blk.MaximumVolumes);
-	printf("BlockRange Address - %lu\n", super_blk.BlockedOutOfRange.First);
-	printf("BlockRange Count - %lu\n", super_blk.BlockedOutOfRange.Count);
-	printf("MappingTreeIdent - %lu\n", super_blk.MappingTreeIdent);
-	printf("OtherFlags - %lX\n", super_blk.OtherFlags);
-	printf("JumpstartEFI Address - %lu\n", super_blk.JumpstartEFI);
-	printf("KeyLocker Address - %lu\n", super_blk.KeyLocker.First);
-	printf("KeyLocker Count - %lu\n", super_blk.KeyLocker.Count);
-	printf("EphemeralInfo 1 - %lu\n", super_blk.EphemeralInfo[0]);
-	printf("EphemeralInfo 2 - %lu\n", super_blk.EphemeralInfo[1]);
-	printf("EphemeralInfo 3 - %lu\n", super_blk.EphemeralInfo[2]);
-	printf("EphemeralInfo 4 - %lu\n", super_blk.EphemeralInfo[3]);
+	printf("BlockSize - %u\n", super_blk->BlockSize);
+	printf("BlocksCount - %lu\n", super_blk->BlocksCount);
+	printf("Features - %lu\n", super_blk->Features);
+	printf("ReadOnlyFeatures - %lu\n", super_blk->ReadOnlyFeatures);
+	printf("IncompatibleFeatures - %lu\n", super_blk->IncompatibleFeatures);
+	printf("NextIndent - %lu\n", super_blk->NextIdent);
+	printf("NextTransaction- %lu\n", super_blk->NextTransaction);
+	printf("Desc Blocks - %u\n", super_blk->DescriptorBlocks);
+	printf("Data Blocks - %u\n", super_blk->DataBlocks);
+	printf("DescriptorBase - %lu\n", super_blk->DescriptorBase);
+	printf("DataBase - %lu\n", super_blk->DataBase);
+	printf("DescriptorNext - %u\n", super_blk->DescriptorNext);
+	printf("DataNext - %u\n", super_blk->DataNext);
+	printf("DescriptorIndex - %u\n", super_blk->DescriptorIndex);
+	printf("DescriptorLength - %u\n", super_blk->DescriptorLength);
+	printf("DataIndex - %u\n", super_blk->DataIndex);
+	printf("DataLength - %u\n", super_blk->DataLength);
+	printf("SpaceManagerIdent - %lu\n", super_blk->SpaceManagerIdent);
+	printf("ObjectsMapIdent - %lu\n", super_blk->ObjectsMapIdent);
+	printf("ReaperIdent - %lu\n", super_blk->ReaperIdent);
+	printf("MaximumVolumes - %u\n", super_blk->MaximumVolumes);
+	printf("BlockRange Address - %lu\n", super_blk->BlockedOutOfRange.First);
+	printf("BlockRange Count - %lu\n", super_blk->BlockedOutOfRange.Count);
+	printf("MappingTreeIdent - %lu\n", super_blk->MappingTreeIdent);
+	printf("OtherFlags - %lX\n", super_blk->OtherFlags);
+	printf("JumpstartEFI Address - %lu\n", super_blk->JumpstartEFI);
+	printf("KeyLocker Address - %lu\n", super_blk->KeyLocker.First);
+	printf("KeyLocker Count - %lu\n", super_blk->KeyLocker.Count);
+	printf("EphemeralInfo 1 - %lu\n", super_blk->EphemeralInfo[0]);
+	printf("EphemeralInfo 2 - %lu\n", super_blk->EphemeralInfo[1]);
+	printf("EphemeralInfo 3 - %lu\n", super_blk->EphemeralInfo[2]);
+	printf("EphemeralInfo 4 - %lu\n", super_blk->EphemeralInfo[3]);
 	printf("-------------------------------------\n");
+
+	return super_blk;
 }
 
 void parse_APFS( int block_no )
 {
 	FILE *apfs = NULL;
 	char filename[16] = {0};
+	APFS_BH *block_header;
+	APFS_SuperBlk *super_blk;
 
 	snprintf(filename, sizeof(filename), "decompressed%d", block_no);
 
@@ -86,12 +91,12 @@ void parse_APFS( int block_no )
 		return;
 	}
 	
-	if (parse_blk_header(apfs)) {
+	if (NULL == (block_header = parse_blk_header(apfs))) {
 		printf("Unable to parse APFS: Failed to parse Block header\n");
 		return;
 	}
 	
-	if (parse_super_blk(apfs)) {
+	if (NULL == (super_blk = parse_super_blk(apfs))) {
 		printf("Unable to parse APFS: Failed to parse Suoper Block\n");
 		return;
 	}
